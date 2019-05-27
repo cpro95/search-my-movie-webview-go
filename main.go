41a97c85bc84702f47d14c94488fc6a6aff9cff0
cpro95 <cpro95@gmail.com>
2019-05-27 13:06:45 +0900

    go static server with embed data with go-bindata-assertfs

--- main.go
+++ main.go
@@ -1,62 +1,34 @@
 package main
 
 import (
-	"flag"
-	"log"
 	"net/http"
-	"strings"
+	"log"
 
 	"github.com/zserge/webview"
 )
 
-// FileSystem custom file system handler
-type FileSystem struct {
-	fs http.FileSystem
-}
-
-const (
-	windowWidth  = 800
-	windowHeight = 900
-)
-
-// Open opens file
-func (fs FileSystem) Open(path string) (http.File, error) {
-	f, err := fs.fs.Open(path)
-	if err != nil {
-		return nil, err
-	}
-
-	s, err := f.Stat()
-	if s.IsDir() {
-		index := strings.TrimSuffix(path, "/") + "/index.html"
-		if _, err := fs.fs.Open(index); err != nil {
-			return nil, err
+func startServer() string {
+	go func() {
+		http.Handle("/", http.FileServer(assetFS()))
+		
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal("ListenAndServe: ", err)
 		}
-	}
-
-	return f, nil
+	}()
+	return "http://localhost:8080"
 }
+
 
 func main() {
-	port := flag.String("p", "9999", "port to serve on")
-	directory := flag.String("d", "./build", "the directory of static file to host")
-	flag.Parse()
 
-	go func() {
-		fileServer := http.FileServer(FileSystem{http.Dir(*directory)})
-		http.Handle("/", http.StripPrefix(strings.TrimRight("/", "/"), fileServer))
-		log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
-		log.Fatal(http.ListenAndServe(":"+*port, nil))
-	}()
-
+	url := startServer()
 	w := webview.New(webview.Settings{
-		Width:     windowWidth,
-		Height:    windowHeight,
-		Title:     "Simple",
-		Resizable: true,
-		URL:       "http://127.0.0.1:9999",
+		Width:  800,
+		Height: 980,
+		Title:  "Search My Movie",
+		URL:    url,
 	})
-	w.SetColor(255, 255, 255, 255)
 	defer w.Exit()
 	w.Run()
 }
